0c97d34edf2d4c0c2a6d4e582ef9b89c38dbaf8d
Raj10203 <patilricky39@gmail.com>
2025-08-06 13:00:42 +0530

    change permissions of storage folder

--- database/migrations/2025_08_06_061015_create_categories_table.php
+++ database/migrations/2025_08_06_061015_create_categories_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->renameColumn('name', 'category_name');
             $table->string('slug');
             $table->string('description')->nullable();
             $table->timestamps();
@@ -25,6 +26,10 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->renameColumn('category_name', 'name');
+        });
+
         Schema::dropIfExists('categories');
     }
 };
--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+Artisan::command('raj', function () {
+    $this->comment(Inspiring::quote());
+});
